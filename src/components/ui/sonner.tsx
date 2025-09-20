2c6cd3e2e253a9e28b635ff1707b421ebf6492dc
naufal hafizhan <naufal@naufal.com>
2025-09-20 14:17:19 +0700

    fix: adjust dark and light mode

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -1,25 +1,26 @@
 "use client"
 
-import { useTheme } from "next-themes"
+import { useTheme } from "@/context/ThemeContext"
+// import { useTheme } from "next-themes"
 
 import { Toaster as Sonner, ToasterProps } from "sonner"
 // import { useTheme } from "@/context/ThemeContext"
 
 const Toaster = ({ ...props }: ToasterProps) => {
-  const { theme = "system" } = useTheme()
-  // const { theme } = useTheme()
-
+  const { theme } = useTheme()
+  const toastTheme = (theme === "light") ? "dark" : "light"
   return (
     <Sonner
-      theme={theme  as ToasterProps["theme"]}
+      theme={toastTheme as ToasterProps["theme"]}
       className="toaster group"
-      style={
-        {
-          "--normal-bg": "var(--popover)",
-          "--normal-text": "var(--popover-foreground)",
-          "--normal-border": "var(--border)",
-        } as React.CSSProperties
-      }
+      // style=
+      // {
+      //   {
+      //     "--normal-bg": "var(--popover)",
+      //     "--normal-text": "var(--popover-foreground)",
+      //     "--normal-border": "var(--border)",
+      //   } as React.CSSProperties
+      // }
       {...props}
     />
   )
