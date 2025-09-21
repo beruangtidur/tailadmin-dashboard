fbfeb924c5decf59ad4896f5c57087f5429cfeb2
nauyusimi <nauyusimi@gmail.com>
2025-09-22 06:32:30 +0700

    feat: bcrypt comparison password

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,10 +1,9 @@
-import NextAuth from 'next-auth'
-import { PrismaAdapter } from '@auth/prisma-adapter'
 import { prisma } from '@/lib/prisma'
+import { PrismaAdapter } from '@auth/prisma-adapter'
+import NextAuth from 'next-auth'
 import type { Provider } from 'next-auth/providers'
 import Credentials from 'next-auth/providers/credentials'
-import bcrypt from 'bcrypt'
-import { isValidDate } from '@fullcalendar/core/internal'
+import * as bcrypt from 'bcrypt';
 
 const providers: Provider[] = [
     Credentials({
@@ -14,8 +13,8 @@ const providers: Provider[] = [
             // console.log(c)
             const user = await prisma.user.findUnique({ where: { email: c.email } })
             if (!user) return null
-            // const isValid = await bcrypt.compare(c.password, user.password)
-            const isValid = c.password === user.password
+            const isValid = await bcrypt.compare(c?.password, user.password)
+            // const isValid = c.password === user.password
             if (!isValid) return null
 
             return user
